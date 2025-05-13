d58ae7a0bc3808e327e4f0db44a692c11ae4d6c0
AlvinLeeGitHub <new.lee@hotmail.com>
2025-05-13 23:38:08 +0800

    Add root route '/' to return welcome message

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// 配置根路由
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// 配置其他路由
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
